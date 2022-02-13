90ff361ff0b84855a93815e20a69380521868be8
Ali A. Dhillon <aleedhx@gmail.com>
2022-02-13 18:20:11 +0500

    set the user agent or IB

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -24,6 +24,7 @@ class User
     protected $phone;
     protected $phone_password;
     protected $investor_password;
+    protected $agent;
 
     /**
      * @return mixed
@@ -226,5 +227,8 @@ class User
         return $this;
     }
 
-
+    public function setAgent($agentLogin)
+    {
+        $this->agent = $agentLogin;
+    }
 }
