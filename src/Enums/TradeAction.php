a54df280d502b720ec9f45ee15c8fdd9016a0b86
Tarikh Agustia <agustia.tarikh150@gmail.com>
2023-02-07 07:06:54 +0700

    fix: wrong enum

--- src/Enums/TradeAction.php
+++ src/Enums/TradeAction.php
@@ -5,5 +5,5 @@ namespace Tarikh\PhpMeta\Enums;
 class TradeAction
 {
     const TA_DEALER_POS_EXECUTE = 200;   
-    const TA_DEALER_ORD_PENDING = 200;   
+    const TA_DEALER_ORD_PENDING = 201;   
 }
